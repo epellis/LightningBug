c2a8412a25d4c660ccbff0d5b14c5a97f8aca41b
epellis <nedellis1234@gmail.com>
2021-05-03 22:39:15 -0700

    ...

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
+function Bolt({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default Bolt
