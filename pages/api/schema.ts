ce2f38f78d2df352e063c1faffe17950a99211e7
epellis <nedellis1234@gmail.com>
2021-05-04 23:38:29 -0700

    ...

--- pages/api/schema.ts
+++ pages/api/schema.ts
@@ -1,38 +1,16 @@
-import { objectType, queryType, makeSchema, stringArg, nonNull, idArg, mutationType } from 'nexus'
-import { nexusPrisma } from 'nexus-plugin-prisma'
-import path from 'path'
-import prisma from "./prisma"
+import { Func } from 'nexus-prisma'
+import { makeSchema, objectType } from 'nexus'
 
 export const schema = makeSchema({
-  plugins: [
-    nexusPrisma({ experimentalCRUD: true })
-  ],
-  outputs: {
-    typegen: path.join(process.cwd(), 'pages/api/nexus-typegen.ts'),
-    schema: path.join(process.cwd(), 'pages/api/schema.graphql'),
-  },
-  // contextType: {
-  //   module: require.resolve('.prisma/client/index.d.ts'),
-  //   export: 'PrismaClient',
-  // },
-  sourceTypes: {
-    modules: [
-      {
-        module: '@prisma/client',
-        alias: 'prisma',
-      },
-    ],
-  },
   types: [
-    // queryType({
-    //   definition(t) {
-    //     t.crud.func()
-    //   }
-    // }),
-    // mutationType({
-    //   definition(t) {
-    //     t.crud.createOneFunc()
-    //   }
-    // })
-  ],
+    objectType({
+      name: Func.$name,
+      description: Func.$description,
+      definition(t) {
+        t.field(Func.id.name, Func.id)
+        t.field(Func.name.name, Func.name)
+        t.field(Func.contents.name, Func.contents)
+      }
+    })
+  ]
 })
