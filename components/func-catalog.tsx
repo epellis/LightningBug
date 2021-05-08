b166c6b5e31aa1b5813cf2cee6f5b0a270ba8ff6
epellis <nedellis1234@gmail.com>
2021-05-07 19:20:48 -0700

    ...

--- components/func-catalog.tsx
+++ components/func-catalog.tsx
@@ -2,16 +2,6 @@ import { Func } from '@prisma/client';
 import React, { useMemo } from 'react'
 import { useTable } from 'react-table'
 
-// async function getFuncs(url): Promise<Func[]> {
-//   console.log("Get Funcs")
-//   return (await axios.get<Func[]>(url)).data
-// }
-
-// export async function getStaticProps() {
-//   const initialFuncs = await getFuncs("/api/funcs")
-//   return { props: { initialFuncs } }
-// }
-
 export default function FuncCatalog({ funcs }: { funcs: Func[] }) {
   const columns = useMemo(() => [
     {
