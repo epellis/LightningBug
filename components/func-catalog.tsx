ff02343bbc5428bcb2c1687bad87972f687bf22d
epellis <nedellis1234@gmail.com>
2021-05-07 19:11:41 -0700

    ...

--- components/func-catalog.tsx
+++ components/func-catalog.tsx
@@ -36,6 +36,31 @@ export default function FuncCatalog({ funcs }: { funcs: Func[] }) {
 
   return <table {...getTableProps()}>
     <thead>
-    </thead>
-  </table>
+      {headerGroups.map(headerGroup => (
+        <tr {...headerGroup.getHeaderGroupProps()}>
+          {headerGroup.headers.map(column => (
+            <th {...column.getHeaderProps}>
+              {column.render('Header')}
+            </th>
+          ))}
+        </tr>
+      ))}
+    </thead >
+    <tbody {...getTableBodyProps()}>
+      {rows.map(row => {
+        prepareRow(row)
+        return (
+          <tr {...row.getRowProps()}>
+            {row.cells.map(cell => {
+              return (
+                <td {...cell.getCellProps()}>
+                  {cell.render('Cell')}
+                </td>
+              )
+            })}
+          </tr>
+        )
+      })}
+    </tbody>
+  </table >
 }
